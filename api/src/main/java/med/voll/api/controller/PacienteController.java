f96ae191460f7a090082252b3085f41c8312f759
Medeiros000 <98979459+Medeiros000@users.noreply.github.com>
2023-06-06 14:37:00 -0300

    Paciente (Get, Put, Del e Post) adicionado

--- api/src/main/java/med/voll/api/controller/PacienteController.java
+++ api/src/main/java/med/voll/api/controller/PacienteController.java
@@ -1,18 +1,41 @@
 package med.voll.api.controller;
 
-import med.voll.api.paciente.DadosCadastroPaciente;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import jakarta.transaction.Transactional;
+import jakarta.validation.Valid;
+import med.voll.api.paciente.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping ("/pacientes")
 public class PacienteController {
 
+    @Autowired
+    private PacienteRepository repository;
     @PostMapping
+    @Transactional
     public void cadastrar(@RequestBody DadosCadastroPaciente dados){
+        repository.save(new Paciente(dados));
         System.out.println("dados recebidos" + dados);
     }
-
+    @GetMapping
+    public Page<DadosListagemPaciente> listar(@PageableDefault(size = 10, sort ={"nome"}) Pageable paginacao){
+        System.out.println("listando");
+        return repository.findAllByAtivoTrue(paginacao).map(DadosListagemPaciente::new);
+    }
+    @PutMapping
+    @Transactional
+    public void atualizar(@RequestBody @Valid DadosAtualizacaoPaciente dados){
+        var paciente = repository.getReferenceById(dados.id());
+        paciente.atualizarInformacoes(dados);
+    }
+    @DeleteMapping("/{id}")
+    @Transactional
+    public void excluir(@PathVariable Long id){
+        var paciente = repository.getReferenceById(id);
+        paciente.excluir();
+    }
 }
