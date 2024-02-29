3fd064c1dbc346d3f13a3715ab9d0ed67d7dbc10
Júnior Medeiros <98979459+Medeiros000@users.noreply.github.com>
2024-02-28 23:46:48 -0300

    vercel 2

--- api/src/main/resources/static/js/jquery.js
+++ api/src/main/resources/static/js/jquery.js
@@ -34,10 +34,10 @@ function doSearch(searchTerm) {
   if (searchTerm == null || searchTerm == "") {
     // Se estiver vazio, faça uma chamada à API para obter todos os resultados
     // Substitua a URL abaixo pela URL real da sua API
-    apiUrl = `https://voll-med-medeiros000s-projects.vercel.app/html/${currentPage}`;
+    apiUrl = `https://voll-med-medeiros000s-projects.vercel.app/${currentPage}`;
   } else {
     // Se não estiver vazio, faça uma chamada à API para obter os resultados filtrados
-    apiUrl = `https://voll-med-medeiros000s-projects.vercel.app/html/${currentPage}/${searchTerm}`;
+    apiUrl = `https://voll-med-medeiros000s-projects.vercel.app/${currentPage}/${searchTerm}`;
   }
   // console.log(apiUrl);
   // Fazer a chamada à API usando fetch
